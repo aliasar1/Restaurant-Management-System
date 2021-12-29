91d2c0b3a7f5df2b16e17a2549bd6f8ed2eff48b
Bilal Pervez <rajabilal555@gmail.com>
2021-12-29 21:28:55 +0500

    Added BreakFast menu and some alignment and other stuff

--- Restaurant_Management_System 1.asm
+++ Restaurant_Management_System 1.asm
@@ -70,7 +70,7 @@ mainMenu        db 10,13,'        MAIN',10,13
                 db 'Press 1 for Breakfast',10,13
                 db 'Press 2 for Lunch',10,13
                 db 'Press 3 for Dinner',10,13
-                db 'Press 0 to return to Exit',10,13,'$'
+                db 'Press 0 to Exit',10,13,'$'
 
 
 ;           MENU
@@ -79,84 +79,113 @@ menuMenu        db 10,13,'        MENU',10,13
                 db 'Press 1 for Starters',10,13
                 db 'Press 2 for Main Course',10,13
                 db 'Press 3 for Desert',10,13
-                db 'Press 4 to return to Main',10,13,'$'
+                db 'Press 4 to Return to Main Menu',10,13,'$'
 
 ; Update Menu
 updateMenu      db 10,13,'        UPDATE MENU',10,13
                 db 'Press 1 for Add to Menu',10,13
                 db 'Press 2 for Delete from Menu',10,13
-                db 'Press 3 to return to Main',10,13,'$'   
+                db 'Press 3 to Return to Main Menu',10,13,'$'   
+
+;BreakFast MENU
+BreakFastStarterMenu db 10,13,'        STARTER MENU',10,13
+                db 'Press 1 for Latte                       Rs 80',10,13
+                db 'Press 2 for Espresso                    Rs 90',10,13
+                db 'Press 3 for Tea                         Rs 20',10,13
+                db 'Press 4 for Tea Karak                   Rs 30',10,13 
+                db 'Press 5 for Green Tea                   Rs 20',10,13
+                db 'Press 6 for Orange Juice                Rs 50',10,13
+                db 'Press 0 to go back',10,13,'$'
+
+BreakFastMainCourseMenu db 10,13,'      MAIN COURSE MENU',10,13
+                db 'Press 1 for Omelette Meal               Rs 80',10,13
+                db 'Press 2 for Egg Muffin Meal             Rs 60',10,13
+                db 'Press 3 for Hot Cakes                   Rs 40',10,13
+                db 'Press 4 for Halwa Puri                  Rs 20',10,13  
+                db 'Press 5 for Alu Paratha.                Rs 20',10,13
+                db 'Press 6 for Egg & Hashbrown Wrap.       Rs 80',10,13
+                db 'Press 7 for Qeema paratha               Rs 30',10,13
+                db 'Press 8 for Chicken Karahi              Rs 80',10,13
+                db 'Press 0 to go back',10,13,'$'
+
+BreakFastDessertMenu  db 10,13,'         DESSERT MENU',10,13
+                db 'Press 1 for Double Chocolate Pie.       Rs 80',10,13
+                db 'Press 2 for Apple Pie.                  Rs 40',10,13
+                db 'Press 3 for Nutella Brownie.            Rs 60',10,13
+                db 'Press 4 for Chocolate Cake.             Rs 60',10,13
+                db 'Press 5 for Cheese Cake.                Rs 80',10,13
+                db 'Press 6 for Mocktails.                  Rs 60',10,13,
+                db 'Press 0 to go back',10,13,'$'
 
 ;LUNCH MENU
 LunchStarterMenu db 10,13,'        STARTER MENU',10,13
-                db 'Press 1 for French Fries. Rs 20',10,13
-                db 'Press 2 for Cheesy fries. Rs 20',10,13
-                db 'Press 3 for Samosa Beef. Rs 20',10,13
-                db 'Press 4 for Hummus. Rs 20',10,13 
-                db 'Press 5 for Spicy Mexican Wings. Rs 30',10,13
-                db 'Press 6 for Chicken Corn Soup. Rs 20',10,13
-                db 'Press 0 for return to back',10,13,'$'
-                
+                db 'Press 1 for French Fries.               Rs 20',10,13
+                db 'Press 2 for Cheesy fries.               Rs 20',10,13
+                db 'Press 3 for Samosa Beef.                Rs 20',10,13
+                db 'Press 4 for Hummus.                     Rs 20',10,13 
+                db 'Press 5 for Spicy Mexican Wings.        Rs 30',10,13
+                db 'Press 6 for Chicken Corn Soup.          Rs 20',10,13
+                db 'Press 0 to go back',10,13,'$'
+
 LunchMainCourseMenu db 10,13,'      MAIN COURSE MENU',10,13
-                db 'Press 1 for Lahori Fish. Rs 40',10,13
-                db 'Press 2 for French Onion Steak. Rs 40',10,13
-                db 'Press 3 for Fajita Steak. Rs 40',10,13
-                db 'Press 4 for Chicken Biryani. Rs 60',10,13  
-                db 'Press 5 for Mutton Pulao. Rs 60',10,13
-                db 'Press 6 for Whole Chicken Roasted. Rs 80',10,13
-                db 'Press 7 for Stuffed Beef Whole Rs 80',10,13
-                db 'Press 8 for Roasted Turkey. Rs 80',10,13
-                db 'Press 0 to return to back',10,13,'$'
-                
+                db 'Press 1 for Lahori Fish.                Rs 40',10,13
+                db 'Press 2 for French Onion Steak.         Rs 40',10,13
+                db 'Press 3 for Fajita Steak.               Rs 40',10,13
+                db 'Press 4 for Chicken Biryani.            Rs 60',10,13  
+                db 'Press 5 for Mutton Pulao.               Rs 60',10,13
+                db 'Press 6 for Whole Chicken Roasted.      Rs 80',10,13
+                db 'Press 7 for Stuffed Beef Whole          Rs 80',10,13
+                db 'Press 8 for Roasted Turkey.             Rs 80',10,13
+                db 'Press 0 to go back',10,13,'$'
+
 LunchDessertMenu  db 10,13,'         DESSERT MENU',10,13
-                db 'Press 1 for Shahi Tukra. Rs 20',10,13
-                db 'Press 2 for Fudge Brownie. Rs 20',10,13
-                db 'Press 3 for Flavored Icecream. Rs 20',10,13
-                db 'Press 4 for Choclate Moose Cake. Rs 60',10,13
-                db 'Press 5 for Ferrero Roacher Cake. Rs 80',10,13
-                db 'Press 6 for Machiatos. Rs 40',10,13,
-                db 'Press 0 to return to back',10,13,'$'                          
-                
+                db 'Press 1 for Shahi Tukra.                Rs 20',10,13
+                db 'Press 2 for Fudge Brownie.              Rs 20',10,13
+                db 'Press 3 for Flavored Icecream.          Rs 20',10,13
+                db 'Press 4 for Choclate Moose Cake.        Rs 60',10,13
+                db 'Press 5 for Ferrero Roacher Cake.       Rs 80',10,13
+                db 'Press 6 for Machiatos.                  Rs 40',10,13,
+                db 'Press 0 to go back',10,13,'$'                          
 
 
-;DINNER MENU                
+
+;DINNER MENU
 DinnerStarterMenu db 10,13,'        STARTER MENU',10,13
-                db 'Press 1 for Bacon rings. Rs 20',10,13
+                db 'Press 1 for Bacon rings.                Rs 20',10,13
                 db 'Press 2 for Prawn and ginger dumplings. Rs 40',10,13
-                db 'Press 3 for Tuna wings. Rs 60',10,13
-                db 'Press 4 for Sweet french fries Rs 20',10,13 
-                db 'Press 5 for Buffalo Wings Rs 60',10,13
-                db 'Press 6 for Mirchi Pakora Rs 20',10,13
-                db 'Press 0 for return to back',10,13,'$'
+                db 'Press 3 for Tuna wings.                 Rs 60',10,13
+                db 'Press 4 for Sweet french fries          Rs 20',10,13 
+                db 'Press 5 for Buffalo Wings               Rs 60',10,13
+                db 'Press 6 for Mirchi Pakora               Rs 20',10,13
+                db 'Press 0 to go back',10,13,'$'
                 
 DinnerMainCourseMenu db 10,13,'      MAIN COURSE MENU',10,13
-                db 'Press 1 for Peshawari Chapli Kebab. Rs 60',10,13
-                db 'Press 2 for Haleem. Rs 60',10,13
-                db 'Press 3 for Nalli Nihari. Rs 80',10,13
-                db 'Press 4 for Paya Curry. Rs 40',10,13  
-                db 'Press 5 for Channa Pindi. Rs 40',10,13
-                db 'Press 6 for Lamb Rogan Josh. Rs 80',10,13
-                db 'Press 7 for Butter chicken Rs 60',10,13
-                db 'Press 8 for Goat Feet. Rs 80',10,13
-                db 'Press 0 to return to back',10,13,'$'
+                db 'Press 1 for Peshawari Chapli Kebab.     Rs 60',10,13
+                db 'Press 2 for Haleem.                     Rs 60',10,13
+                db 'Press 3 for Nalli Nihari.               Rs 80',10,13
+                db 'Press 4 for Paya Curry.                 Rs 40',10,13  
+                db 'Press 5 for Channa Pindi.               Rs 40',10,13
+                db 'Press 6 for Lamb Rogan Josh.            Rs 80',10,13
+                db 'Press 7 for Butter chicken              Rs 60',10,13
+                db 'Press 8 for Goat Feet.                  Rs 80',10,13
+                db 'Press 0 to go back', 10,13,'$'
                 
 DinnerDessertMenu  db 10,13,'         DESSERT MENU',10,13
-                db 'Press 1 for Gulab Jamun. Rs 20',10,13
-                db 'Press 2 for Ice Cream. Rs 40',10,13
-                db 'Press 3 for Nutella Brownie. Rs 60',10,13
-                db 'Press 4 for Chocolate Cake. Rs 60',10,13
-                db 'Press 5 for Cheese Cake. Rs 80',10,13
-                db 'Press 6 for Mocktails. Rs 60',10,13,
-                db 'Press 0 to return to back',10,13,'$'                          
-                
-;Quantity                                             
+                db 'Press 1 for Gulab Jamun.                Rs 20',10,13
+                db 'Press 2 for Ice Cream.                  Rs 40',10,13
+                db 'Press 3 for Nutella Brownie.            Rs 60',10,13
+                db 'Press 4 for Chocolate Cake.             Rs 60',10,13
+                db 'Press 5 for Cheese Cake.                Rs 80',10,13
+                db 'Press 6 for Mocktails.                  Rs 60',10,13,
+                db 'Press 0 to go back', 10,13,'$'
 
-quantityPrompt db 10,13,'Quantity: $'   
+;Quantity
+quantityPrompt db 10,13,'Quantity: $'
 
-price db 10,13,'Total Price: $'    
+price db 10,13,'Total Price: $'
 
 backMainMenu db 10,13,10,13,'1.Go Back to Main Menu$'
-exitPrompt db 10,13,'0.Exit$'                        
+exitPrompt db 10,13,'0.Exit$'
 
 .code
 
