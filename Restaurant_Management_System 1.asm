4a045d110a66d867c3154fc7465f562b158e2859
Ali Asar Khowaja <khowajaaliasar@gmail.com>
2022-01-02 01:10:41 +0500

    Added admin validation
    Implemented username and password validation to access system.

--- Restaurant_Management_System 1.asm
+++ Restaurant_Management_System 1.asm
@@ -1,4 +1,5 @@
-; Username and password validation to use system
+; Username and password validation to use system  
+
 check macro v1,v2,v3,v4,v5,v6 
     local L1, check1, compare, wrong, right, print_star, L2  
     lea dx, v1
@@ -166,7 +167,6 @@ menuMenu        db 10,13,'        MENU',10,13
                 db 'Press 1 for Starters',10,13
                 db 'Press 2 for Main Course',10,13
                 db 'Press 3 for Dessert',10,13
-                db 'Press 3 for Desert',10,13
                 db 'Press 4 to Return to Main Menu',10,13,'$'
 
 ; Update Menu
@@ -183,11 +183,6 @@ BreakFastStarterMenu db 10,13,'        STARTER MENU',10,13
                 db 'Press 4 for Tea Karak                   Rs 40',10,13 
                 db 'Press 5 for Green Tea                   Rs 20',10,13
                 db 'Press 6 for Orange Juice                Rs 40',10,13
-                db 'Press 2 for Espresso                    Rs 90',10,13
-                db 'Press 3 for Tea                         Rs 20',10,13
-                db 'Press 4 for Tea Karak                   Rs 30',10,13 
-                db 'Press 5 for Green Tea                   Rs 20',10,13
-                db 'Press 6 for Orange Juice                Rs 50',10,13
                 db 'Press 0 to go back',10,13,'$'
 
 BreakFastMainCourseMenu db 10,13,'      MAIN COURSE MENU',10,13
@@ -198,7 +193,6 @@ BreakFastMainCourseMenu db 10,13,'      MAIN COURSE MENU',10,13
                 db 'Press 5 for Alu Paratha.                Rs 20',10,13
                 db 'Press 6 for Egg & Hashbrown Wrap.       Rs 80',10,13
                 db 'Press 7 for Qeema paratha               Rs 40',10,13
-                db 'Press 7 for Qeema paratha               Rs 30',10,13
                 db 'Press 8 for Chicken Karahi              Rs 80',10,13
                 db 'Press 0 to go back',10,13,'$'
 
@@ -739,6 +733,7 @@ Exit:
     int 21h
     
     main endp 
+    
     
 ; Helper procs    
 star proc
